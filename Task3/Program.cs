d686f5f22dcd6623a9e510d92dffbe0a912f8308
Iryna <batsianiryna@gmail.com>
2022-12-08 23:26:12 +0300

    add task3 генерация массива случайных вещественных чисел и находим разницу между большим элементом и меньшим

---
+++ Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void InputArray(double[] array)  
+{ 
+  for (int i = 0; i < array.Length; i++) 
+      array[i] = Math.Round(new Random().NextDouble()*(20 - 10) + 10, 2);    
+} 
+ 
+ 
+double MaxArray(double[] array) 
+{ 
+  double maxArray = array[0];
+  for (int i = 1; i < array.Length; i++)  
+    { 
+      if(maxArray < array[i]) 
+        maxArray = array[i]; 
+    } 
+    return maxArray;
+} 
+
+
+double MinArray(double[] array) 
+{ 
+  double minArray = array[0];
+  for (int i = 1; i < array.Length; i++)  
+    { 
+      if(minArray > array[i]) 
+        minArray = array[i]; 
+    } 
+    return minArray;
+} 
+ 
+ 
+Console.Clear(); 
+Console.Write("Введите количество элементов массива: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+double[] array = new double[n]; 
+InputArray(array);  
+Console.WriteLine($"Полученный массив: [{string.Join(", ", array)}]");  
+Console.WriteLine($"Разница между максимальным и минимальным элементом данного массива:  {MaxArray(array) - MinArray(array)}"); 
+
