14c7959559d3f0ddc7430ef5e436d460875699ff
Iryna <batsianiryna@gmail.com>
2022-12-08 09:03:46 +0300

    add task2 сумма элементов нечетных позиций

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+ 
+ void InputArray(int[] array) // Процедура для заполнения массива
+{
+  for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(100, 1000);   
+}
+
+
+int ReleaseArray(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++) // Прохождение по массиву
+  {
+     if (array[i] % 2 == 0)
+     count++;
+  } 
+  return count;
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n]; // Создаем сам массив в котором количество элементов = n.
+InputArray(array); // Процедура заполнения нашего одномерного массива
+Console.WriteLine($"[{string.Join(", ", array)}]"); // Выводим заданный массив
+Console.WriteLine($"В данном массиве четных чисел: {ReleaseArray(array)}"); // Запускаем процедуру
---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+ void InputArray(int[] array) // Процедура для заполнения массива
+{
+  for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(-100, 100);   
+}
+
+
+int ReleaseArray(int[] array)
+{
+  int sum = 0;
+  for (int i = 1; i < array.Length; i += 2) // Прохождение по массиву
+     sum += array[i];
+  
+  return sum;
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n]; // Создаем сам массив в котором количество элементов = n.
+InputArray(array); // Процедура заполнения нашего одномерного массива
+Console.WriteLine($"[{string.Join(", ", array)}]"); // Выводим заданный массив
+Console.WriteLine($"Сумма элементов нечетных позиций: {ReleaseArray(array)}"); // Запускаем процедуру
